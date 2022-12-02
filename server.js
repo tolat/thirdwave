3f21d2c3e2e21ab30c06b8ccae2ccb6568fc27e4
macuser <macuser@macusers-Mac-mini.local>
2022-12-01 16:11:23 -0800

    updated minor styling and added date fields to quote modal

--- server.js
+++ server.js
@@ -30,12 +30,59 @@ app.use((req, res, next) => {
   handleCORS(req, res, next);
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+// POST route for sending emailed message from the Contact form
+app.post("/message", async (req, res) => {
+  try {
+    await transporter.sendMail({
+      from: req.body.email,
+      to: "heliosolarsystems@gmail.com",
+      subject: "Message From Heliosite ",
+      text: `
+      Name: ${req.body.name}\n
+      Phone: ${req.body.phone}\n
+      Email: ${req.body.email}\n\n
+      ${req.body.message}`,
+    });
+  } catch (e) {
+    console.log(e);
+    res.send({ error: e });
+  }
+
+  res.send({ success: true });
+});
+
+// POST route for sending emailed quote request from the free quote form
+app.post("/quote", async (req, res) => {
+  try {
+    await transporter.sendMail({
+      from: req.body.userEmail,
+      to: "heliosolarsystems@gmail.com",
+      subject: "Quote Request From Heliosite",
+      text: `
+      Project Address: ${req.body.userAddress}\n
+      Client Name: ${req.body.userName}\n
+      Phone: ${req.body.userPhone}\n
+      Email: ${req.body.userEmail}\n\n
+      Roof Material: ${req.body.roofMaterial}\n
+      Roof Age: ${req.body.roofAge}\n
+      Roof Pitch: ${req.body.roofPitch}\n
+      Target Offset: ${req.body.targetOffset}\n
+      Average Bill: ${req.body.averageBill}\n
+      Billing Cycle: ${req.body.billingCycle}\n
+      System Type: ${req.body.systemType}\n
+      Battery Backup: ${req.body.batteryBackup}\n
+      Budget: ${req.body.budget}\n`,
+    });
+  } catch (e) {
+    console.log(e);
+    res.send({ error: e });
+  }
+
+  res.send({ success: true });
 });
 
 app.get("*", (req, res) => {
-  res.send("no path matched");
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 app.listen(process.env.PORT, () => {
